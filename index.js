7f49c47a60b56ba38c3cfe4b71d8e6330546aa29
Katie Lundell <katie.a.s.lundell@gmail.com>
2024-08-24 19:57:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ $(document).on("keypress", function (){
 }
 });
 
-$(document).on("tap", function (){
+$(document).on("touch", function (){
     if (!started){
         $("h2").remove();
         $("#level-title").text("Level " + level);
